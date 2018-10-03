9702ffe26bd7982dfe6a62b2d0a368b0e840d30b
Koy Saeteurn <kosatun@aol.com>
2018-10-03 20:37:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  attr_accessor :name, :blog 
+  
+  def initialize(params)
+    @name = params[:name]
+    @blog = params[:blog]
+  end   
 end
